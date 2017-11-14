780ecdadea24603cdd53567e7257992c6e861c18
serena100615 <serena100615@gmail.com>
2017-11-14 10:49:36 +0800

    Guess

--- app/src/main/java/com/example/user/guess100/MainActivity.java
+++ app/src/main/java/com/example/user/guess100/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.user.guess100;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,13 +16,35 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);TextView secret = (TextView) findViewById(R.id.secret);
-        TextView info = (TextView) findViewById(R.id.info);
-        EditText number = (EditText) findViewById(R.id.number);
-        Button send = (Button) findViewById(R.id.send);
+        setContentView(R.layout.activity_main);
+        TextView secret = (TextView) findViewById(R.id.secret);
+        secret.setText(Integer.toString(n));
     }
-public void send (View view){
+public void send (View view) {
+    TextView info = (TextView) findViewById(R.id.info);
+    EditText number = (EditText) findViewById(R.id.number);
+    Button send = (Button) findViewById(R.id.send);
+    int num = Integer.parseInt(number.getText().toString());
+
+        int a = 0;
+        int b = 100;
+        if (num < n) {
+            new AlertDialog.Builder(this)
+                    .setMessage(num + "~" + b)
+                    .setPositiveButton("OK", null)
+                    .show();
+        } else if (num > n) {
+            new AlertDialog.Builder(this)
+                    .setMessage(a + "~" + num)
+                    .setPositiveButton("OK", null)
+                    .show();
+        } else {
+            new AlertDialog.Builder(this)
+                    .setMessage("Bingo!!!")
+                    .setPositiveButton("OK", null)
+                    .show();
+
+        }
 
 }
-
 }
