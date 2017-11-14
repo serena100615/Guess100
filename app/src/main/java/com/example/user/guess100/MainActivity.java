224d888b79ece408d06736fc93f26d298cae8c6d
serena100615 <serena100615@gmail.com>
2017-11-14 10:22:27 +0800

    findViews

--- app/src/main/java/com/example/user/guess100/MainActivity.java
+++ app/src/main/java/com/example/user/guess100/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.user.guess100;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    int n = new Random().nextInt(100)+1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);TextView secret = (TextView) findViewById(R.id.secret);
+        TextView info = (TextView) findViewById(R.id.info);
+        EditText number = (EditText) findViewById(R.id.number);
+        Button send = (Button) findViewById(R.id.send);
     }
+public void send (View view){
+
+}
+
 }
